44ef3ed5dfd090d3241a16f4ab812f42560f0d25
elchin123321 <elchiniskanderov@gmail.com>
2021-04-03 23:28:57 +0300

    LR6
    LR6

---
+++ Proj1/Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,39 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.NickName = "Player" + Random.Range(1000, 9999);
+        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.GameVersion = "1";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Log("Connect to Master");
+    }
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(null, new Photon.Realtime.RoomOptions {MaxPlayers=2 });
+    }
+    public void JoinRandRoom()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Log("Connect the room");
+        PhotonNetwork.LoadLevel("PhotonRoom");
+    }
+    private void Log(string msg)
+    {
+        Debug.Log(msg);
+    }
+}
---
+++ Proj1/Assets/Scripts/PGameManager.cs
@@ -0,0 +1,36 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PGameManager : MonoBehaviourPunCallbacks
+{
+    public GameObject PlayerPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.Instantiate(PlayerPrefab.name, new Vector3(0,0),Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Leave()
+    {
+        PhotonNetwork.LeaveRoom(); 
+    }
+
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene("LobbyPhoton");
+    }
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        
+    }
+}
